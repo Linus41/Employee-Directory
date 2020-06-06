5a9b5055f5974c806d09caec90e96338b41824bb
Linus41 <schief.craft@gmail.com>
2020-06-06 14:40:59 -0700

    initial commit of react app

---
+++ src/components/InputForm/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function InputForm(props) {
+    return (
+        <div className="inputForm">
+            <form>
+                <h4>Search Directory</h4>
+                <input
+                    type="text"
+                />
+            </form>
+        </div>
+    );
+}
+
+export default InputForm; 
