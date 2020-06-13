7bc179828cc4b7ff40dcef1a72dccfa084988729
Linus41 <schief.craft@gmail.com>
2020-06-12 22:47:03 -0700

    got department drop-down to render list 1-12 which isn't what I want but it's something

--- src/components/SearchBy/index.js
+++ src/components/SearchBy/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
 //create global variable that filters departments and then use it in by department code
-const uniqueDept = 
+const uniqueDept = [];
+console.log(uniqueDept);
 
 function SearchBy(props) {
     return (
         // filter by name
-
         <div className="searchBy">
             <h4>Search:</h4>
             <form>
@@ -22,9 +22,13 @@ function SearchBy(props) {
             <form>
                 <label>By Department:</label>
                 <select>
+                    {/* trying to take the duplicates out of the department array, but is there a department array??
+                    Need to push to global array from here, then use set ?*/}
+                   <option></option>
                    
-                    <option>{props.employees.filter((employee) => employee)}</option>
-                      
+                        {props.employees.map(department => (
+                        <option>{uniqueDept.push(department.department)}</option>
+                        ))};
                     
                 </select>
             </form>
