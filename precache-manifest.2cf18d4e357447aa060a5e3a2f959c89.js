d9a8e218a20958afde109e3c4a110d63a8b1a4c6
Linus41 <schief.craft@gmail.com>
2020-06-24 21:32:07 -0700

    Updates

---
+++ precache-manifest.2cf18d4e357447aa060a5e3a2f959c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47909518dc1e0ec7bb3953640ff783ca",
+    "url": "/React-Friend-Directory/index.html"
+  },
+  {
+    "revision": "c96c165a4ef840df09b3",
+    "url": "/React-Friend-Directory/static/css/main.bfbb8fd1.chunk.css"
+  },
+  {
+    "revision": "ebf3b106a7a1cca11e7a",
+    "url": "/React-Friend-Directory/static/js/2.7d4a13c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Friend-Directory/static/js/2.7d4a13c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96c165a4ef840df09b3",
+    "url": "/React-Friend-Directory/static/js/main.1cc59d35.chunk.js"
+  },
+  {
+    "revision": "8d748de5878d970b14ce",
+    "url": "/React-Friend-Directory/static/js/runtime-main.bc6c4007.js"
+  }
+]);
